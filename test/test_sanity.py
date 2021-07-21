4c6e9e1b9a283c733811c7ad4fea9e730b65dcdf
Kevin <kevin.scott.98@gmail.com>
2021-07-21 20:53:58 +0200

    New tests for encoding Point and generating a bitcoin address on testnet

--- test/test_sanity.py
+++ test/test_sanity.py
@@ -1,5 +1,5 @@
 import bitcoin
-from common import Point, P
+import common
 
 def test_ecc_mul():
     G1 = bitcoin.Point(
@@ -7,13 +7,56 @@ def test_ecc_mul():
         y = 0x483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8
     )
 
-    G2 = Point(
+    G2 = common.Point(
         x = 0x79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798,
         y = 0x483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8
     )
 
     G1 = G1 * 1337
-    G1 = Point(G1.x, G1.y)
+    G2 = G2 * 1337
 
-    assert G1 == G2 * 1337
+    G1 = common.Point(G1.x, G1.y)
+    assert G1 == G2
 
+def test_encode_small_point():
+    G1 = bitcoin.Point(
+        x = 5,
+        y = 6
+    )
+
+    G2 = common.Point(
+        x = 5,
+        y = 6
+    )
+
+    assert len(G1.encode()) == len(G2.encode())
+    assert G1.encode() == G2.encode()
+
+def test_encode_big_point():
+    G1 = bitcoin.Point(
+        x = 0x79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798,
+        y = 0x483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8
+    )
+
+    G2 = common.Point(
+        x = 0x79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798,
+        y = 0x483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8
+    )
+
+    assert len(G1.encode()) == len(G2.encode())
+    assert G1.encode() == G2.encode()
+
+
+def test_generate_bitcoin_addr():
+    secret_key = int.from_bytes(b'Hello world', 'big')
+    # Generator point
+    G = bitcoin.Point(
+        x = 0x79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798,
+        y = 0x483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8
+    )
+    public_key = G * secret_key
+
+    # Generate our bitcoin address
+    address = public_key.address()
+
+    assert address == 'mtuFXC3oACRqVMMqN32L5VG7ZCbaE7aZxi'
